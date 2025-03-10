9484c399506c18fe61e92c10af65227df24e6808
Veritt <lucasmanyvongsa@gmail.com>
2025-03-10 14:48:02 -0400

    more notes

--- Text-Based-RPG/obj/Debug/net8.0/Text-Based-RPG.AssemblyInfo.cs
+++ Text-Based-RPG/obj/Debug/net8.0/Text-Based-RPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Text-Based-RPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72dded12bcdf661c7a09d9647ab7fbcb97110653")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c688b3376960760121eaabd00f96c5547902ac1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Text-Based-RPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Text-Based-RPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
